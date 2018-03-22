d96a9a53305017a1072d6766bc4661f974550f9e
Rodrigo Martín de Lamo <rmartide@everis.com>
2018-03-22 12:44:10 +0100

    Added car service with method get thats obtains all cars from the api. Added cars component and a simple view to test the method.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HttpInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { CarService } from './car.service';
+import { CarsComponent } from './cars/cars.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    HttpInMemoryWebApiModule.forRoot(
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, {dataEncapsulation: false}
     )
     
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 //Class to mock the REST calls
+//The address will be /api/cars
 
 @Injectable()
 export class InMemoryDataService implements InMemoryDbService {
@@ -11,44 +12,45 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         Id: 1,
         Brand: 'Mercedes',
-        Registration: '1999-01-08 04:05:06',
+        Registration: '1521718105744',
         Country: 'Germany',
-        Created_at: '1999-01-08 04:05:06',
-        Last_updated: '1999-01-08 04:05:06'
+        Created_at: '1521718105744',
+        Last_updated: '1521718105744'
       },
       {
         Id: 2,
         Brand: 'BMW',
-        Registration: '2000-01-08 04:05:06',
+        Registration: '1421718105744',
         Country: 'Germany',
-        Created_at: '2000-01-08 04:05:06',
-        Last_updated: '2000-01-08 04:05:06'
+        Created_at: '1421718105744',
+        Last_updated: '1421718105744'
       },
       {
         Id: 3,
         Brand: 'Renault',
-        Registration: '2001-01-08 04:05:06',
+        Registration: '1321718105744',
         Country: 'France',
-        Created_at: '2001-01-08 04:05:06',
-        Last_updated: '2001-01-08 04:05:06'
+        Created_at: '1321718105744',
+        Last_updated: '1321718105744'
       },
       {
         Id: 4,
         Brand: 'Seat',
-        Registration: '2002-01-08 04:05:06',
+        Registration: '1221718105744',
         Country: 'Spain',
-        Created_at: '2002-01-08 04:05:06',
-        Last_updated: '2002-01-08 04:05:06'
+        Created_at: '1221718105744',
+        Last_updated: '1221718105744'
       },
       {
         Id: 5,
         Brand: 'Toyota',
-        Registration: '2003-01-08 04:05:06',
+        Registration: '1121718105744',
         Country: 'Japan',
-        Created_at: '2003-01-08 04:05:06',
-        Last_updated: '2003-01-08 04:05:06'
+        Created_at: '1121718105744',
+        Last_updated: '1121718105744'
       }
     ]
+
 
     return {cars};
   }
